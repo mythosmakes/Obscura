7c0d12b9051f7c0ec3b722be4fdd952b8ca340be
mixedmercury <112225985+mixedmercury@users.noreply.github.com>
2023-10-07 00:36:54 -0400

    Movement Coded

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Character Input Values")]
+    public Vector2 move;
+
+    public void OnMove(InputValue value)
     {
-        
+        MoveInput(value.Get<Vector2>());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveInput(Vector2 newMoveDirection)
     {
-        
+        move = newMoveDirection;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,10 +11,25 @@ public class PlayerController : MonoBehaviour
     public GameObject mirrorShard1;
     public GameObject mirrorShard2;
     public GameObject mirrorShard3;
+    
+    CharacterController characterController;
+    private InputManager input;
+    private PlayerInput playerInput;
 
+    public float moveSpeed;
+    private float targetRotation = 0.0f;
+    private float verticalSpeed;
+    private Vector3 characterVelocity;
+    private float rotationVelocity;
+    [Range(0.0f, 0.3f)]
+    public float turningTime = 0.12f;
 
     private void Start()
     {
+        characterController = GetComponent<CharacterController>(); // get character controller
+        input = GetComponent<InputManager>();
+        playerInput = GetComponent<PlayerInput>();
+        
         mirrorShard1.SetActive(false);
         mirrorShard2.SetActive(false);
         mirrorShard3.SetActive(false);
@@ -23,11 +39,22 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Simple controls to get player moving, delete later for touch controls
-        var inputX = Input.GetAxis("Horizontal");
-        var inputY = Input.GetAxis("Vertical");
-        Vector3 moveVector = new Vector3(-inputY * inputX, 0, inputX * inputY) * movementSpeed * Time.deltaTime;
+        float targetSpeed = moveSpeed;
+        if (input.move == Vector2.zero) targetSpeed = 0.0f;
+        float currentSpeed = new Vector3(characterController.velocity.x, 0.0f, characterController.velocity.z).magnitude;
+        float inputMagnitude = input.move.magnitude; //if using analog stick, scale with input. Else, magnitude is 1f
 
-        transform.Translate(moveVector);
+        Vector3 inputDirection = new Vector3(input.move.x, 0.0f, input.move.y).normalized;
+        if (input.move != Vector2.zero)
+        {
+            targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + 45;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, turningTime);
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            Debug.Log("Move input!");
+        }
+        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+        characterVelocity = targetDirection.normalized * (targetSpeed * Time.deltaTime) + new Vector3(0.0f, verticalSpeed, 0.0f);
+        characterController.Move(targetDirection.normalized * (targetSpeed * Time.deltaTime) + new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime); //move player
     }
 
     public void UpdateShards()
